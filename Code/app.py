657dcc9274d267ca8d310d0840e8ac520241bb67
DenEnesteFugl <kjq112@alumni.ku.dk>
2025-05-25 11:40:22 +0200

    Update app.py
    Har lavet todo om til guess

--- Code/app.py
+++ Code/app.py
@@ -6,7 +6,7 @@ import sqlite3
 def db_connection():
     # When we run this code we will this file being created. The file will persist between executations of the server.
     # Keep in mind that you may need to delete this file every time you change the schema of your database.
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect('guess.db')
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -14,15 +14,15 @@ def db_connection():
 def init_db():
     conn = db_connection()
     # We create a table that has two fields: the id of the todo, and a todo_text that is unique
-    conn.execute('''CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo_text TEXT NOT NULL UNIQUE)''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS guesses (id INTEGER PRIMARY KEY AUTOINCREMENT, guess_text TEXT NOT NULL UNIQUE)''')
 
     # This cursor a database bureaucracy: it is a control structure that enables traversal over the records in a database.
     c = conn.cursor()
-    todos = ['DIS assignment 1', 'Groceries', 'DIS assignment 2', 'DIS project']
-    for todo in todos:
+    guesses = ['DIS assignment 1', 'Groceries', 'DIS assignment 2', 'DIS project']
+    for guess in guesses:
         # (todo, ) is a Python quirk: we need to provide tuples to the insert query, and that is how we can define a tuple with a single element (an 1-nary tuple).
         # The OR IGNORE is a trick so when we run the database again we don't get errors from duplicated entries (as we have the UNIQUE constraint).
-        c.execute('INSERT OR IGNORE INTO todos (todo_text) VALUES (?)', (todo,))
+        c.execute('INSERT OR IGNORE INTO guesses (guess_text) VALUES (?)', (guess,))
 
     conn.commit()
     conn.close()
@@ -38,23 +38,23 @@ def hello_world():
 
 # We have a new route to /todo
 @app.route('/guess', methods=['GET', 'POST'])
-def list_todo():
+def list_guess():
     conn = db_connection()
     # This route has two functions: GET and POST.
     # If there is a POST, we do the insert in the database
     if request.method == 'POST':
-        new_todo = request.form['new_todo']
+        new_guess = request.form['new_guess']
         c = conn.cursor()
-        c.execute('INSERT OR IGNORE INTO todos (todo_text) VALUES (?)', (new_todo,))
+        c.execute('INSERT OR IGNORE INTO guesses (guess_text) VALUES (?)', (new_guess,))
         conn.commit()
 
     # But we always get all entries from the database
-    db_todos = conn.execute('SELECT todo_text FROM todos').fetchall()
+    db_guesses = conn.execute('SELECT guess_text FROM guesses').fetchall()
     conn.close()
-    todos = []
+    guesses = []
     # The entries come in dictionary data structure, we need to convert it to a list
-    for db_todo in db_todos:
-        todos.append(db_todo['todo_text'])
+    for db_guess in db_guesses:
+        guesses.append(db_guess['guess_text'])
 
-    # We render the todo template with todos we fetched from the database
-    return render_template('todo.html', todos=todos)
+    # We render the todo template with guesses we fetched from the database
+    return render_template('guess.html', guesses=guesses)
